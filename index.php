62e9db0c6098c38b18a8a57a1189bdcf14143367
ADarkHero <maik.riedlsperger@gmail.com>
2020-01-29 10:06:27 +0100

    Fixed index.php

--- index.php
+++ index.php
@@ -10,5 +10,21 @@
 		->setClientSecret($api_secret)
 		->build();
 		
-	$client->orderUnits()->send(314567863347035, "DHL", "00340434281548406657");
+	
+	
+	$file = fopen('tracking.csv', 'r');
+
+	while (($line = fgetcsv($file, 0, ";")) !== FALSE) {
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $apiurl); //Add url to curl	
+		$response = curl_exec($curl); //Call url via curl
+			
+		var_dump($response); //Show response
+		echo "<br>";
+		
+		
+		$client->orderUnits()->send($line[0], $line[1], $line[2]);
+	}
+	
+	fclose($file);
 ?>
